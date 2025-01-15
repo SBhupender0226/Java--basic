acb1eac3c4c32daee2af0582fa4cb6920aaf1a52
SBhupender0226 <bhupendersingh0226@gmail.com>
2025-01-15 22:14:20 +0530

    old

--- src/day_1.java
+++ src/day_1.java
@@ -1,6 +1,6 @@
 public class day_1 {
     public static void main(String[] args) {
-        System.out.println("This is my new code.");
+        System.out.println("This is my old code.");
         System.out.println("Hello, java");
     }
 }
