18e3c5cf061e413191930df5380d3d8b5a6cab46
SBhupender0226 <bhupendersingh0226@gmail.com>
2025-01-15 22:39:40 +0530

    adada

--- src/day_1.java
+++ src/day_1.java
@@ -1,7 +1,7 @@
 public class day_1 {
     public static void main(String[] args) {
         System.out.println("This is my old code.");
-        System.out.println("yo yo ");
+        System.out.println("yo yo git config --global credential.helper");
         System.out.println("Hello, java");
     }
 }
