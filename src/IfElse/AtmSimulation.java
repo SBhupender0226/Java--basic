5bde2e27a17bd3b51fec179422a9d679489e9898
SBhupender0226 <bhupendersingh0226@gmail.com>
2025-01-30 23:49:02 +0530

    none

--- src/IfElse/AtmSimulation.java
+++ src/IfElse/AtmSimulation.java
@@ -6,7 +6,7 @@ public class AtmSimulation {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         double balance = 10000;
-        System.out.print("Enter the amount you want to withdraw : ");
+        System.out.print("Enter the amount you want to withdraw: ");
         double withdraw = sc.nextDouble();
         if((withdraw>0)&&(withdraw%100==0)){
             if (withdraw<=balance){
