89218e24a5a8f97935e00c251fa88a511f851ad0
SBhupender0226 <bhupendersingh0226@gmail.com>
2025-02-03 22:47:13 +0530

    task 22

---
+++ src/IfElse/Task_22.java
@@ -0,0 +1,24 @@
+package IfElse;
+
+import java.util.Scanner;
+
+public class Task_22 {
+    public static void main(String[] args) {
+//        Check number is divisible by 5 and 11
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        int num = sc.nextInt();
+        if ((num%5==0)&&(num%11==0)){
+            System.out.println("Number is divisible by 5 and 11 both");
+        }
+        else if (num%5==0){
+            System.out.println("Number is divisible by 5");
+        }
+        else if (num%11==0){
+            System.out.println("Number is divisible by 11");
+        }
+        else{
+            System.out.println(" Not divisible.");
+        }
+    }
+}
